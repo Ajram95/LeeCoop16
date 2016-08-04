da32af145e0a25fa2179b1846d4be6cf6d0b504d
Arjun Ramaswamy <ramaswamy.a@husky.neu.edu>
2016-08-04 15:48:22 -0400

    Add files via upload

---
+++ tissue_background_subtraction.m
@@ -0,0 +1,34 @@
+clear,clc
+cd('E:\LeeLab-Coop1\test 6 frozen')
+load('new_sub_bkgd1')
+New1 = [24 397 849 902];
+for m = 1
+    New = New1(m,:);
+    I2=zeros(1,900);
+    I2(30:900)=(sum(I_tissue(30:900,New)')/length(New));
+end
+
+cd('E:\LeeLab-Coop1\test 6 frozen')
+load('new_sub_bkgd1')
+clear('para')
+A = [I2; ones(1,900)];
+A1 = [A(:,400:520) A(:,550:570)];
+%A1 = [A(:,300:700)];
+A2 = A(:,30:900);
+for i = 1:960
+    I_tissue(30:900,i) = smooth(I_tissue(30:900,i),11);
+    b = I_tissue(:,i);
+    b = smooth(b,20,'rloess');
+    b = [b(400:520);b(550:570)];
+    %b = [b(300:700)];
+    cvx_begin
+        variable x(2)
+        minimize(norm(b'-x'*A1))
+        subject to
+                min(x) >= 0
+    cvx_end
+    para(1:2,i) = x;
+    I3(30:900,i)=(I_tissue(30:900,i)'-x'*A2).';
+end
+plot(I3)
+save('further_bkgd_procs','I3','para','A2','I_tissue','r1')
